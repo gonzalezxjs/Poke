9a80fe04ece5a3bce3ac2c3dcb684d42ef054f1c
gonzalez9512 <darkfiles.exe@gmail.com>
2021-10-26 12:41:24 -0500

    1.0

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div >
+      Creado por <a href="https://bit.ly/3AXn2x7">Claud</a>
+    </div>
+    
+  </footer>
+);
+
+export default Footer;
